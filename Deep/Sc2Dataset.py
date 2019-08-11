e8b308e804a9d44ad5132566433d8e1e9f5d17c1
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2019-08-11 03:18:33 -0400

    Update Sc2Dataset.py

--- Deep/Sc2Dataset.py
+++ Deep/Sc2Dataset.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 import os
+import random
 
 
 class Sc2Dataset(Dataset):
@@ -16,7 +17,7 @@ class Sc2Dataset(Dataset):
         self.directory_path = f"training_data/train_data_{botname}_{hm_choices}_choices_{hm_unit_class}_units"
         assert os.path.exists(self.directory_path), f"The dir {self.directory_path} doesn't exist"
 
-        self.data = list()
+        self.full_data = list()
         self.get_data()
         temp_path = self.directory_path
         self.directory_path = "../" + self.directory_path
@@ -25,33 +26,34 @@ class Sc2Dataset(Dataset):
         else:
             self.directory_path = temp_path
 
+        self.data = list()
+        self.balance_data()
+        random.shuffle(self.data)
+
     def __len__(self):
-        return len(self.data)
+        return min(10_000, len(self.data))
 
     def __getitem__(self, i):
         return [torch.from_numpy(self.data[i][0]), np.argmax(self.data[i][1])]
 
-    # def balance_data(self):
-    #     data_counter = {str(d): 0 for d in set(targets)}
-    #     data_container = {str(d): [] for d in set(targets)}
-    #     for i, tar in enumerate(targets):
-    #         data_counter[str(tar)] += 1
-    #         data_container[str(tar)].append((ts[i], tar))
-    #     min_count = min(data_counter.values())
-    #
-    #     # print(data_counter)
-    #
-    #     new_ts = []
-    #     for t in data_container.values():
-    #         new_ts += [d[0] for d in t][:min_count]
-    #     new_tar = []
-    #     for t in data_container.values():
-    #         new_tar += [d[1] for d in t][:min_count]
-    #
-    #     return np.array(new_ts), np.array(new_tar)
+    def balance_data(self):
+        data_counter = dict()
+        for d in self.full_data:
+            key = np.argmax(d[1])
+            if key in data_counter:
+                data_counter[key]["data"].append(d)
+                data_counter[key]["counter"] += 1
+            else:
+                data_counter[key] = {"data": [d], "counter": 1}
+
+        mean_counter = int(np.mean([info["counter"] for _, info in data_counter.items()]))
+        min_counter = min([info["counter"] for _, info in data_counter.items()])
+        print(min_counter, mean_counter)
+        for key, info in data_counter.items():
+            self.data.extend(info["data"][:mean_counter])
 
     def get_data(self):
-        for i, filename in enumerate(os.listdir(self.directory_path)):
+        for _, filename in enumerate(os.listdir(self.directory_path)):
             game_data = np.load(self.directory_path + "/" + filename, allow_pickle=True).item()
             for data in game_data[self.data_key]:
                 [img, tar] = data
@@ -59,7 +61,7 @@ class Sc2Dataset(Dataset):
                     img = img[np.newaxis, :, :]
                 if sum(tar) > 1:
                     print(tar)
-                self.data.append([img, tar])
+                self.full_data.append([img, tar])
 
 
 if __name__ == "__main__":
